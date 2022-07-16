378f40e6cd0ef949f0a065fef6aab1206b0c3656
NuttaphonChanpan <chanpan.nuttaphon@gmail.com>
2022-07-17 03:41:08 +0700

    Updates

---
+++ precache-manifest.41dcea1edab88a601b3ac781d99d8d87.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d92ddf8d91eb5d64aae1",
+    "url": "/nobody5607/qrcode-scan.git/static/css/main.87180d77.chunk.css"
+  },
+  {
+    "revision": "d92ddf8d91eb5d64aae1",
+    "url": "/nobody5607/qrcode-scan.git/static/js/main.89550dd0.chunk.js"
+  },
+  {
+    "revision": "152d58a639cd22063fd0",
+    "url": "/nobody5607/qrcode-scan.git/static/js/runtime~main.68cd175f.js"
+  },
+  {
+    "revision": "65ab72b97a94051e1399",
+    "url": "/nobody5607/qrcode-scan.git/static/js/2.1a7e90f8.chunk.js"
+  },
+  {
+    "revision": "6754db35381cfc84c77c",
+    "url": "/nobody5607/qrcode-scan.git/static/js/3.ff57d029.chunk.js"
+  },
+  {
+    "revision": "efb82057626aecc8e3e6",
+    "url": "/nobody5607/qrcode-scan.git/static/js/4.055fc431.chunk.js"
+  },
+  {
+    "revision": "2888a82b149a0b3fad24583e1f07dee3",
+    "url": "/nobody5607/qrcode-scan.git/index.html"
+  }
+];
